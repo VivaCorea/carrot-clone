c9483ca2b147c8ebd346ac475a00c0f3900b14aa
VivaCorea <103165098+VivaCorea@users.noreply.github.com>
2024-08-06 22:21:01 +0900

    #6.2

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,7 +1,37 @@
 "use server";
 import { z } from "zod";
 
-const usernameScheme = z.string().min(5).max(10);
+//const usernameScheme = z.string().min(5).max(10);
+const chkUserName = (name: string) => {
+  return !name.includes("potato");
+};
+
+const chkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formScheme = z
+  .object({
+    userName: z
+      .string({
+        invalid_type_error: "type!! is not string",
+        required_error: "name is required",
+      })
+      .min(3, "Way tto short!")
+      .max(10, "That is Too Loooooong")
+      .refine(chkUserName, "no 감자 allowed"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(chkPasswords, {
+    message: "Both password should be same~",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -10,5 +40,15 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  usernameScheme.parse(data.userName);
+
+  /* try {
+    formScheme.parse(data);
+  } catch (e) {
+    console.log(e);
+  } */
+
+  const rst = formScheme.safeParse(data);
+  if (!rst.success) {
+    return rst.error.flatten();
+  }
 }
