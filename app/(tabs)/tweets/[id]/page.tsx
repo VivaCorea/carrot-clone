cbdd5e32bed45a637870e0a5cd2e2cd4ac73a2df
VivaCorea <103165098+VivaCorea@users.noreply.github.com>
2024-08-13 21:37:02 +0900

    #10.8

--- app/(tabs)/tweets/[id]/page.tsx
+++ app/(tabs)/tweets/[id]/page.tsx
@@ -1,12 +1,85 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 60000));
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
 }
 
-export default async function ProductDetail({
-  params: { id },
+async function getTweet(id: number) {
+  const tweet = await db.tweet.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return tweet;
+}
+
+export default async function TweetDetail({
+  params,
 }: {
   params: { id: string };
 }) {
-  const product = await getProduct();
-  return <span>Product detail of the product {id}</span>;
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+  const tweet = await getTweet(id);
+  if (!tweet) {
+    return notFound();
+  }
+  const isOwner = await getIsOwner(tweet.user_id);
+  return (
+    <div>
+      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 rounded-full overflow-hidden">
+          {tweet.user.avatar !== null ? (
+            <Image
+              src={tweet.user.avatar}
+              width={40}
+              height={40}
+              alt={tweet.user.username}
+            />
+          ) : (
+            <UserIcon />
+          )}
+        </div>
+        <div>
+          <h3>{tweet.user.username}</h3>
+        </div>
+      </div>
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold">Contents</h1>
+        <p>{tweet.tweet}</p>
+      </div>
+      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+        {isOwner ? (
+          <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+            Delete tweet
+          </button>
+        ) : null}
+        <Link
+          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+          href={``}
+        >
+          Like
+        </Link>
+      </div>
+    </div>
+  );
 }
--- app/(tabs)/tweets/page.tsx
+++ app/(tabs)/tweets/page.tsx
@@ -1,85 +1,28 @@
+import TweetList from "@/components/tweet-list";
 import db from "@/lib/db";
-import getSession from "@/lib/session";
-import { UserIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
+import { Prisma } from "@prisma/client";
 
-async function getIsOwner(userId: number) {
-  const session = await getSession();
-  if (session.id) {
-    return session.id === userId;
-  }
-  return false;
-}
-
-async function getTweet(id: number) {
-  const tweet = await db.tweet.findUnique({
-    where: {
-      id,
+async function getInitialTweets() {
+  const products = await db.tweet.findMany({
+    select: {
+      tweet: true,
+      created_at: true,
+      id: true,
     },
-    include: {
-      user: {
-        select: {
-          username: true,
-          avatar: true,
-        },
-      },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
     },
   });
-  return tweet;
+  return products;
 }
+export type InitialTweets = Prisma.PromiseReturnType<typeof getInitialTweets>;
 
-export default async function TweetDetail({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const id = Number(params.id);
-  if (isNaN(id)) {
-    return notFound();
-  }
-  const tweet = await getTweet(id);
-  if (!tweet) {
-    return notFound();
-  }
-  const isOwner = await getIsOwner(tweet.user_id);
+export default async function Tweets() {
+  const initialTweets = await getInitialTweets();
   return (
     <div>
-      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
-        <div className="size-10 rounded-full overflow-hidden">
-          {tweet.user.avatar !== null ? (
-            <Image
-              src={tweet.user.avatar}
-              width={40}
-              height={40}
-              alt={tweet.user.username}
-            />
-          ) : (
-            <UserIcon />
-          )}
-        </div>
-        <div>
-          <h3>{tweet.user.username}</h3>
-        </div>
-      </div>
-      <div className="p-5">
-        <h1 className="text-2xl font-semibold">Contents</h1>
-        <p>{tweet.tweet}</p>
-      </div>
-      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
-        {isOwner ? (
-          <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
-            Delete tweet
-          </button>
-        ) : null}
-        <Link
-          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
-          href={``}
-        >
-          Like
-        </Link>
-      </div>
+      <TweetList initialTweets={initialTweets} />
     </div>
   );
 }
